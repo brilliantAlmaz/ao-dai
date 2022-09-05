9428f39a9d9fb692ed4a6dccb8ae038a0525f1e1
Tukhvatulin Almaz <mewlalabla@gmail.com>
2022-09-05 21:27:10 +0300

    update
    slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+		$('html').toggleClass('lock');
 	});
 });
+const welcomeImageblock = document.querySelector('.welcome__imageblock');
+
+window.addEventListener('resize', init);
+var checkImageblock=true;
+init();
+function init(){
+	if (window.screen.width < 601 && checkImageblock){
+		welcomeImageblock.previousElementSibling.querySelector('.btn').insertAdjacentHTML(
+			'beforebegin',
+			`<div class="welcome__imageblock">
+			<img src="img/welcome/01.png" alt="">
+			<img src="img/welcome/02.png" alt="">
+			</div>`
+			);
+		checkImageblock = false;
+		welcomeImageblock.style.display='none';
+	}
+}
+
+const sliderLineExhibition = document.querySelector('.exhibition__slider-line');
+const sliderExhibition = document.querySelector('.exhibition__slider');
+const sliderItemExhibition = document.querySelectorAll('.exhibition__item');
+console.log(sliderItemExhibition);
+sliderItemExhibition.forEach(i => i.addEventListener('click', function(){
+	for (let index=0; index<sliderItemExhibition.length; index++){
+		sliderItemExhibition[index].classList.remove('active');
+	}
+	i.classList.add('active');
+
+}));
