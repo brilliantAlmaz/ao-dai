51991252184bb9434bca0d62a210b163b9b77bf1
Tukhvatulin Almaz <mewlalabla@gmail.com>
2022-09-08 20:08:18 +0300

    update
    explore section slider
    exhibition section slider and addaptive

--- js/script.js
+++ js/script.js
@@ -6,9 +6,108 @@ $(document).ready(function() {
 });
 const welcomeImageblock = document.querySelector('.welcome__imageblock');
 
-window.addEventListener('resize', init);
+
 var checkImageblock=true;
+var checkWindowblock=false;
+
+const sliderLineExhibition = document.querySelector('.exhibition__slider-line');
+const sliderExhibition = document.querySelector('.exhibition__slider');
+const sliderItemExhibition = document.querySelectorAll('.exhibition__item');
+const exhibitionNextBtn = document.querySelector('.arrow-right')
+const exhibitionPrevBtn = document.querySelector('.arrow-left')
+window.addEventListener('resize', init);
+
+console.log(checkWindowblock)
+var indexActive=-1;
+var count=0; var translateWidth=0;
+sliderItemExhibition.forEach((i,j) => i.addEventListener('click', function(){
+	if (window.screen.width>1000){
+		for (let index=0; index<sliderItemExhibition.length; index++){
+			sliderItemExhibition[index].classList.remove('active');
+		}
+		i.classList.add('active');
+		indexActive=j;
+	}
+}));
+
+
+exhibitionNextBtn.addEventListener('click', function(){
+	console.log(count)
+	count++;
+	if (count==sliderItemExhibition.length){
+		count=0;
+		translateWidth=0;
+	}
+	else{
+		translateWidth=translateWidth+sliderItemExhibition[count-1].clientWidth+80;
+		if (window.screen.width<=1000) {
+			translateWidth-=80;
+		}
+	}
+	sliderExhibition.style.transform=`translate( -${translateWidth}px)`;
+	console.log(translateWidth)
+});
+exhibitionPrevBtn.addEventListener('click', function(){
+	if (count==0){
+		count=4;
+		translateWidth=480*4;
+		if (indexActive!=-1){
+			translateWidth=480*3+580
+		}
+	}
+	else{
+		translateWidth=translateWidth-sliderItemExhibition[count-1].clientWidth-80;
+		if (window.screen.width<=1000) {
+			translateWidth+=80;
+		}
+		count--;
+	}
+	sliderExhibition.style.transform=`translate( -${translateWidth}px)`;
+});
+
+
+//explore slider
+const exploreSlider = document.querySelector('.explore__slider')
+const exploreItemSlider = document.querySelectorAll('.explore__item');
+const exploreImageblock = document.querySelectorAll('explore__imageblock');
+const exploreNextBtn = document.querySelector('.explore__arrow .arrow-right')
+const explorePrevBtn = document.querySelector('.explore__arrow .arrow-left')
+const exploreLineItems = document.querySelectorAll('.explore__navigation-item')
+
+let exploreTranslateWidth;
+
+
+
+let exploreCount=2;
 init();
+exploreNextBtn.addEventListener('click', function(){
+	if (exploreCount == exploreItemSlider.length-1){
+		exploreCount=0;
+	}
+	else{
+		exploreCount++;
+	}
+	for (let i of exploreLineItems){
+		i.classList.remove('active');
+	}
+	exploreLineItems[exploreCount].classList.add('active');
+	exploreSlider.style.transform =`translate(-${exploreCount*exploreTranslateWidth}px)`
+})
+explorePrevBtn.addEventListener('click', function(){
+	if (exploreCount == 0){
+		exploreCount=exploreItemSlider.length-1;
+	}
+	else{
+		exploreCount--;
+	}
+	for (let i of exploreLineItems){
+		i.classList.remove('active');
+	}
+	exploreLineItems[exploreCount].classList.add('active');
+	exploreSlider.style.transform =`translate(-${exploreCount*exploreTranslateWidth}px)`
+})
+
+
 function init(){
 	if (window.screen.width < 601 && checkImageblock){
 		welcomeImageblock.previousElementSibling.querySelector('.btn').insertAdjacentHTML(
@@ -21,16 +120,18 @@ function init(){
 		checkImageblock = false;
 		welcomeImageblock.style.display='none';
 	}
-}
-
-const sliderLineExhibition = document.querySelector('.exhibition__slider-line');
-const sliderExhibition = document.querySelector('.exhibition__slider');
-const sliderItemExhibition = document.querySelectorAll('.exhibition__item');
-console.log(sliderItemExhibition);
-sliderItemExhibition.forEach(i => i.addEventListener('click', function(){
-	for (let index=0; index<sliderItemExhibition.length; index++){
-		sliderItemExhibition[index].classList.remove('active');
+	if (window.screen.width<=1000) {
+		sliderItemExhibition.forEach(i => i.classList.add('active'));
 	}
-	i.classList.add('active');
-
-}));
+	else{
+		sliderItemExhibition.forEach(i => i.classList.remove('active'));
+	}
+	let exploreWidth=0;
+	for (let i of exploreItemSlider){
+		exploreWidth=exploreWidth+ +i.clientWidth;
+		exploreTranslateWidth = i.clientWidth;
+	}
+	
+	exploreSlider.style.transform =`translate(-${2*exploreTranslateWidth}px)`
+	exploreSlider.style.width=`${exploreWidth}px`;
+}
